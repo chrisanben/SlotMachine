e38690216569c16e0496c452a16f0b4e9ebc182f
Chris <chrisanben@gmail.com>
2013-06-07 19:22:32 -0400

    Version 0.4
    Made it work.

--- slot_machine.py
+++ slot_machine.py
@@ -7,11 +7,12 @@
                         a random output based on probability, the end result will most likely end
                         in the user running out of money to bet.
                         
-    VERSION 0.3: Added A Money and Bet Amount Labels
-                    - Bet Labels now display how much user bets
-                    - Removed Slot Labels and replaced them as images
-                    - Buttons have been move slightly
-                    - Imported Berries.png with Transparency
+    VERSION 0.4: Slot Machine Works! (kinda)
+                    - Player Money label accurately remove Bet Amount
+                    - Images are displayed in the slot reels
+                    - If 2 or more images coincide, player gain money. (Unless there is rot)
+                    - Jackpot Label/Amount added, with functionality!
+                    - Removed useless functions
 
     
 """
@@ -50,9 +51,36 @@ class SlotMachine:
         self.reel3_imgTk = ImageTk.PhotoImage(self.reel3_img)
         self.bg_panel.create_image(380, 150, image = self.reel3_imgTk, anchor = NW)
 
-        #Import Berries Image
+        #Import Slot Images
         self.berries_img = Image.open("images/berries.png")
         self.berries_imgTk = ImageTk.PhotoImage(self.berries_img)
+
+        self.rot_img = Image.open("images/rot.png")
+        self.rot_imgTk = ImageTk.PhotoImage(self.rot_img)
+
+        self.seeds_img = Image.open("images/seeds.png")
+        self.seeds_imgTk = ImageTk.PhotoImage(self.seeds_img)
+
+        self.durian_img = Image.open("images/durian.png")
+        self.durian_imgTk = ImageTk.PhotoImage(self.durian_img)
+
+        self.carrot_img = Image.open("images/carrot.png")
+        self.carrot_imgTk = ImageTk.PhotoImage(self.carrot_img)
+
+        self.fruit_img = Image.open("images/fruit.png")
+        self.fruit_imgTk = ImageTk.PhotoImage(self.fruit_img)
+
+        self.egg_img = Image.open("images/egg.png")
+        self.egg_imgTk = ImageTk.PhotoImage(self.egg_img)
+
+        self.ham_img = Image.open("images/ham.png")
+        self.ham_imgTk = ImageTk.PhotoImage(self.ham_img)
+
+        self.waffles_img = Image.open("images/waffles.png")
+        self.waffles_imgTk = ImageTk.PhotoImage(self.waffles_img)
+
+        self.taffy_img = Image.open("images/taffy.png")
+        self.taffy_imgTk = ImageTk.PhotoImage(self.taffy_img)
 
         #Display Bet Amount
         self.betAmount = Label (self.bg_panel)
@@ -94,15 +122,35 @@ class SlotMachine:
         self.cashLabel.configure(anchor="n")
         self.cashLabel.configure(text="Cash:")
 
-        # Display the Bet ALL button
-        self.betAllButton = Button(self.bg_panel)
-        self.betAllButton.place(x=425, y=375, height=46, width=125)
-        self.betAllButton.configure(borderwidth="0")
-        self.betAllButton_img = PhotoImage(file="images/button.gif")
-        self.betAllButton.configure(image=self.betAllButton_img)
-        self.betAllButton.configure(cursor="hand2")
-        self.betAllButton.configure(font=self.buttonFont, compound="center", text="Bet All")
-        self.betAllButton.bind("<Button-1>",self.betALL)
+        #Display Jackpot Amount
+        self.jackpotAmount = Label (self.bg_panel)
+        self.jackpotAmount.place(x=360,y=90,height=35,width=150)
+        self.jackpotAmount.configure(background="#000000")
+        self.jackpotAmount.configure(borderwidth="0")
+        self.jackpotAmount.configure(font=self.buttonFont)
+        self.jackpotAmount.configure(foreground="#ff0000")
+        self.jackpotAmount.configure(anchor="n")
+        self.jackpotAmount.configure(text="1000")
+
+        #Display Jackpot Label
+        self.jackpotLabel = Label (self.bg_panel)
+        self.jackpotLabel.place(x=150, y=90, height=35, width=150)
+        self.jackpotLabel.configure(background="#000000")
+        self.jackpotLabel.configure(borderwidth="0")
+        self.jackpotLabel.configure(font=self.buttonFont)
+        self.jackpotLabel.configure(foreground="#ff0000")
+        self.jackpotLabel.configure(anchor="n")
+        self.jackpotLabel.configure(text="Jackpot:")
+
+        # Display the Bet 1000 button
+        self.bet1000Button = Button(self.bg_panel)
+        self.bet1000Button.place(x=425, y=375, height=46, width=125)
+        self.bet1000Button.configure(borderwidth="0")
+        self.bet1000Button_img = PhotoImage(file="images/button.gif")
+        self.bet1000Button.configure(image=self.bet1000Button_img)
+        self.bet1000Button.configure(cursor="hand2")
+        self.bet1000Button.configure(font=self.buttonFont, compound="center", text="Bet 1000")
+        self.bet1000Button.bind("<Button-1>",self.bet1000)
 
         # Display the Bet 100 button
         self.bet100Button = Button(self.bg_panel)
@@ -142,213 +190,175 @@ class SlotMachine:
         self.spinButton.configure(image=self.spinButton_img)
         self.spinButton.configure(cursor="hand2")
         self.spinButton.configure(font=self.buttonFont, compound="center", text="Spin")
-        #self.spinButton.bind("<Button-1>",self.ResetButtonClick)
-    
+        self.spinButton.bind("<Button-1>",self.spinReels)
+
     def bet100(self, event):
-        Player_Bet = 100
-        self.betAmount["text"] = Player_Bet
+        self.betAmount["text"] = 100
         
     def bet250(self, event):
-        Player_Bet = 250
-        self.betAmount["text"] = Player_Bet
+        self.betAmount["text"] = 250
 
     def bet500(self, event):
-        Player_Bet = 500
-        self.betAmount["text"] = Player_Bet
+        self.betAmount["text"] = 500
 
-    def betALL(self, event):
-        Player_Bet = 1000
-        self.betAmount["text"] = Player_Bet
+    def bet1000(self, event):
+        self.betAmount["text"] = 1000
 
+    def spinReels(self, event):
+        Cash = int(self.cashAmount["text"])
+        Bet = int(self.betAmount["text"])
+        Jackpot = int(self.jackpotAmount["text"])        
 
-    def Reels():
+        if Cash >= 100 and Bet > 0:
+            # [0]Fruit, [1]Fruit, [2]Fruit
+            Bet_Line = [" "," "," "]
+            Outcome = [0,0,0]
+            food = self.rot_imgTk
+            winnings = 0
+            win = False
+
+            if Cash < Bet:
+                Bet = Cash
+            Cash -= Bet
+            Jackpot += Bet/4
+
+            self.bg_panel.delete(ALL)
+            # Display the Slot Machine Image on a Canvas Panel
+            self.bg_panel.create_image(0, 0, image = self.bg_imgTk, anchor = NW)
+
+            #Display the Reel Slot
+            self.bg_panel.create_image(200, 150, image = self.reel1_imgTk, anchor = NW)
+
+            #Display the Reel Slot
+            self.bg_panel.create_image(290, 150, image = self.reel2_imgTk, anchor = NW)
+
+            #Display the Reel Slot
+            self.bg_panel.create_image(380, 150, image = self.reel3_imgTk, anchor = NW)
             
-        # [0]Fruit, [1]Fruit, [2]Fruit
-        Bet_Line = [" "," "," "]
-        Outcome = [0,0,0]
-        
-        # Spin those reels
-        for spin in range(3):
-            Outcome[spin] = random.randrange(1,64,1)
-            # Spin those Reels!
-            if Outcome[spin] >= 1 and Outcome[spin] <=18:   # 40.10% Chance
-                Bet_Line[spin] = "Rot"
-            if Outcome[spin] >= 19 and Outcome[spin] <=28:  # 16.15% Chance
-                Bet_Line[spin] = "Durian"
-            if Outcome[spin] >= 29 and Outcome[spin] <=36:  # 13.54% Chance
-                Bet_Line[spin] = "Seeds"
-            if Outcome[spin] >= 37 and Outcome[spin] <=43:  # 11.98% Chance
-                Bet_Line[spin] = "Berries"
-            if Outcome[spin] >= 44 and Outcome[spin] <=49:  # 7.29%  Chance
-                Bet_Line[spin] = "Carrot"
-            if Outcome[spin] >= 50 and Outcome[spin] <=54:  # 5.73%  Chance
-                Bet_Line[spin] = "Dragon Fruit"
-            if Outcome[spin] >= 55 and Outcome[spin] <=58:  # 3.65%  Chance
-                Bet_Line[spin] = "Cooked Egg"  
-            if Outcome[spin] >= 59 and Outcome[spin] <=61:  # 3.65%  Chance
-                Bet_Line[spin] = "Honey Ham"  
-            if Outcome[spin] >= 62 and Outcome[spin] <=63:  # 3.65%  Chance
-                Bet_Line[spin] = "Waffles"  
-            if Outcome[spin] == 64:                         # 1.56%  Chance
-                Bet_Line[spin] = "Taffy"    
-        return Bet_Line
+            # Spin those reels
+            for spin in range(3):
+                Outcome[spin] = random.randrange(1,64,1)
+                # Spin those Reels!
+                if Outcome[spin] >= 1 and Outcome[spin] <=18:   # 40.10% Chance
+                    Bet_Line[spin] = "Rot"
+                    food = self.rot_imgTk
+                if Outcome[spin] >= 19 and Outcome[spin] <=28:  # 16.15% Chance
+                    Bet_Line[spin] = "Durian"
+                    food = self.durian_imgTk
+                if Outcome[spin] >= 29 and Outcome[spin] <=36:  # 13.54% Chance
+                    Bet_Line[spin] = "Seeds"
+                    food = self.seeds_imgTk
+                if Outcome[spin] >= 37 and Outcome[spin] <=43:  # 11.98% Chance
+                    Bet_Line[spin] = "Berries"
+                    food = self.berries_imgTk
+                if Outcome[spin] >= 44 and Outcome[spin] <=49:  # 7.29%  Chance
+                    Bet_Line[spin] = "Carrot"
+                    food = self.carrot_imgTk
+                if Outcome[spin] >= 50 and Outcome[spin] <=54:  # 5.73%  Chance
+                    Bet_Line[spin] = "Dragon Fruit"
+                    food = self.fruit_imgTk
+                if Outcome[spin] >= 55 and Outcome[spin] <=58:  # 3.65%  Chance
+                    Bet_Line[spin] = "Cooked Egg"
+                    food = self.egg_imgTk
+                if Outcome[spin] >= 59 and Outcome[spin] <=61:  # 3.65%  Chance
+                    Bet_Line[spin] = "Honey Ham"
+                    food = self.ham_imgTk
+                if Outcome[spin] >= 62 and Outcome[spin] <=63:  # 3.65%  Chance
+                    Bet_Line[spin] = "Waffles"
+                    food = self.waffles_imgTk
+                if Outcome[spin] == 64:                         # 1.56%  Chance
+                    Bet_Line[spin] = "Taffy"
+                    food = self.taffy_imgTk
 
-    def is_number(Bet):
-        """ This function Checks if the Bet entered by the user is a valid number """
-        try:
-            int(Bet)
-            return True
-        except ValueError:
-            print("Please enter a valid number or Q to quit")
-            return False
+                if spin == 0:
+                    slot1 = self.bg_panel.create_image(202, 150, image = food, anchor = NW)
+                elif spin == 1:
+                    slot2 = self.bg_panel.create_image(292, 150, image = food, anchor = NW)
+                elif spin == 2:
+                    slot3 = self.bg_panel.create_image(382, 150, image = food, anchor = NW)      
+        
 
-    def pullthehandle(Bet, Player_Money, Jack_Pot):
-        """ This function takes the Player's Bet, Player's Money and Current JackPot as inputs.
-            It then calls the Reels function which generates the random Bet Line results.
-            It calculates if the player wins or loses the spin.
-            It returns the Player's Money and the Current Jackpot to the main function """
-        Player_Money -= Bet
-        Jack_Pot += (int(Bet*.15)) # 15% of the player's bet goes to the jackpot
-        win = False
-        Food_Reel = Reels()
-        Food = Food_Reel[0] + " - " + Food_Reel[1] + " - " + Food_Reel[2]
-        
-        # Match 3
-        if Food_Reel.count("Durian") == 3:
-            winnings,win = Bet*5,True
-        elif Food_Reel.count("Seeds") == 3:
-            winnings,win = Bet*10,True
-        elif Food_Reel.count("Berries") == 3:
-            winnings,win = Bet*25,True
-        elif Food_Reel.count("Carrot") == 3:
-            winnings,win = Bet*50,True
-        elif Food_Reel.count("Dragon Fruit") == 3:
-            winnings,win = Bet*100,True
-        elif Food_Reel.count("Cooked Egg") == 3:
-            winnings,win = Bet*200,True
-        elif Food_Reel.count("Honey Ham") == 3:
-            winnings,win = Bet*400,True
-        elif Food_Reel.count("Waffles") == 3:
-            winnings,win = Bet*800,True
-        elif Food_Reel.count("Taffy") == 3:
-            print("TAFFY!!")
-            winnings,win = Bet*1000,True
-        
-        # Match 2
-        elif Food_Reel.count("Rot") == 0:
-            if Food_Reel.count("Durian") == 2:
-                winnings,win = Bet*1,True
-            if Food_Reel.count("Seeds") == 2:
+            Food_Reel = Bet_Line
+
+            if Food_Reel.count("Durian") == 3:
                 winnings,win = Bet*3,True
-            elif Food_Reel.count("Berries") == 2:
-                winnings,win = Bet*5,True
-            elif Food_Reel.count("Carrot") == 2:
+            elif Food_Reel.count("Seeds") == 3:
+                winnings,win = Bet*6,True
+            elif Food_Reel.count("Berries") == 3:
                 winnings,win = Bet*10,True
-            elif Food_Reel.count("Dragon Fruit") == 2:
+            elif Food_Reel.count("Carrot") == 3:
                 winnings,win = Bet*15,True
-            elif Food_Reel.count("Cooked Egg") == 2:
-                winnings,win = Bet*20,True
-            elif Food_Reel.count("Honey Ham") == 2:
+            elif Food_Reel.count("Dragon Fruit") == 3:
                 winnings,win = Bet*25,True
-            elif Food_Reel.count("Waffles") == 2:
+            elif Food_Reel.count("Cooked Egg") == 3:
                 winnings,win = Bet*50,True
-            elif Food_Reel.count("Taffy") == 2:
+            elif Food_Reel.count("Honey Ham") == 3:
                 winnings,win = Bet*100,True
-        
-            # Match Lucky Seven
-            elif Food_Reel.count("Taffy") == 1:
-                winnings, win = Bet*5,True
-                
-            else:
-                winnings, win = Bet,True
-                
-        if win:    
-            print(Food + "\n" + "You Won $ " + str(int(winnings)) + " !!! \n")
-            Player_Money += int(winnings)
-        
-            # Jackpot 1 in 450 chance of winning
-            jackpot_try = random.randrange(1,51,1)
-            jackpot_win = random.randrange(1,51,1)
-            if  jackpot_try  == jackpot_win:
-                print ("You Won The Jackpot !!!\nHere is your $ " + str(Jack_Pot) + "prize! \n")
-                Jack_Pot = 500
-            elif jackpot_try != jackpot_win:
-                print ("You did not win the Jackpot this time. \nPlease try again ! \n")
-        # No win
-        else:
-            print(Food + "\nPlease try again. \n")
-        
-        return Player_Money, Jack_Pot, win
+            elif Food_Reel.count("Waffles") == 3:
+                winnings,win = Bet*200,True
+            elif Food_Reel.count("Taffy") == 3:
+                print("TAFFY!!")
+                winnings,win = Bet*500,True
+            
+            # Match 2
+            elif Food_Reel.count("Rot") == 0:
+                if Food_Reel.count("Durian") == 2:
+                    winnings,win = Bet*1,True
+                if Food_Reel.count("Seeds") == 2:
+                    winnings,win = Bet*2,True
+                elif Food_Reel.count("Berries") == 2:
+                    winnings,win = Bet*3,True
+                elif Food_Reel.count("Carrot") == 2:
+                    winnings,win = Bet*5,True
+                elif Food_Reel.count("Dragon Fruit") == 2:
+                    winnings,win = Bet*10,True
+                elif Food_Reel.count("Cooked Egg") == 2:
+                    winnings,win = Bet*20,True
+                elif Food_Reel.count("Honey Ham") == 2:
+                    winnings,win = Bet*30,True
+                elif Food_Reel.count("Waffles") == 2:
+                    winnings,win = Bet*50,True
+                elif Food_Reel.count("Taffy") == 2:
+                    winnings,win = Bet*100,True
+            
+                # Match TAFFY!
+                elif Food_Reel.count("Taffy") == 1:
+                    winnings, win = Bet*5,True
+                    
+                else:
+                    winnings, win = Bet,True
 
-    def textSlot():
-        """ The Main function that runs the game loop """
-        # Flag to initiate the game loop
-        KeepGoing = True
-        
-        while KeepGoing == True:
-            win = 0
-            # Give the player some money if he goes broke
-            if Player_Money <1:
-                input("You have no more money. Here is $500 \nPress Enter\n")
-                Player_Money = 500
+
+            if win:    
+                Cash += winnings
             
-            # User Input
-            Prompt = raw_input(" Place Your Bet ! \n Jackpot $ " + str(Jack_Pot) + "\n Money $ " + str(Player_Money) + "\n Q = quit \n")
-            if Prompt  == "q" or Prompt  == "Q":
-                KeepGoing = False
-                break
-            
-            if Prompt == "" and Turn >1:
-                Bet = Prev_Bet
-                print("Using Previous Bet")
-                if Bet > Player_Money:
-                    print("Sorry, you only have $" + str(Player_Money) + " \n")
-                elif Bet <= Player_Money:
-                    Turn +=1
-                    Prev_Bet = Bet
-                    Player_Money, Jack_Pot, win = pullthehandle(Bet, Player_Money, Jack_Pot)
-            
-            elif is_number(Prompt ):
-                Bet = int(Prompt )
-                # not enough money
-                if Bet > Player_Money:
-                    print("Sorry, you only have $" + str(Player_Money) + " \n")
-                    
-                # Let's Play
-                elif Bet <= Player_Money:
-                    Turn +=1
-                    Prev_Bet = Bet
-                    Player_Money, Jack_Pot, win = pullthehandle(Bet, Player_Money, Jack_Pot)
-            
-            # determine win/loss ratio for debugging purposes
-            if win:
-                win_number += 1
+                # Jackpot 1 in 450 chance of winning
+                jackpot_try = random.randrange(1,51,1)
+                jackpot_win = random.randrange(1,51,1)
+                if  jackpot_try  == jackpot_win:
+                    Cash += Jackpot
+                    Jackpot = 1000
+                    print "Win!"
+                elif jackpot_try != jackpot_win:
+                    print "No luck."
             else:
-                loss_number += 1
-            win_ratio = "{:.2%}".format(win_number / Turn)
-            print("Wins: " + str(win_number) + "\nLosses: " + str(loss_number) + "\nWin Ratio: " + win_ratio + "\n")           
-                    
-        
-        #The End
-        print("- Program Terminated -")
+                print("\nPlease try again. \n")
+
+        elif Cash <= 0:
+            Cash += 500
+
+        self.cashAmount["text"] = Cash
+        self.betAmount["text"] = Bet
+        self.jackpotAmount["text"] = Jackpot       
 
 def main():
-    # Initial Values
-    Player_Money = 1000
-    Jack_Pot = 500
-    Turn = 1
-    Bet = 0
-    Prev_Bet=0
-    win_number = 0
-    loss_number = 0
-    
     window = Tk()
     window.title("Don't Starve Slot Machine")
     window.geometry('640x480+532+0')
     window.minsize(width=640,height=480)
     window.maxsize(width=640,height=480)
 
-    myApp = SlotMachine(window)
+    mySlotMachine = SlotMachine(window)
     window.mainloop()
     
 if __name__ == "__main__": main()
